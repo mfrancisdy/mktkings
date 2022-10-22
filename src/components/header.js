a43c53e41fb6aca49d5f0ef38b598035f23d8ff4
Abin G <abings002@gmail.com>
2022-10-22 17:59:20 +0530

    Mobile Menu

--- src/components/header.js
+++ src/components/header.js
@@ -1,40 +1,48 @@
 import React from 'react';
 import { Link } from '@reach/router';
 import MainLogo from '../assets/Images/logomain.svg';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Offcanvas from 'react-bootstrap/Offcanvas';
 
 export default function Header() {
     return (
-        <header className='container navigation-header'>
-            <nav className="navbar navbar-expand-lg">
-                <div className="container-fluid">
-                    <Link className="navbar-brand" to="/"><img src={MainLogo} alt="logo" /></Link>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarNav">
-                        <ul className="navbar-nav navBarItems text-white">
-                            <li className="nav-item">
-                                <Link className="nav-link" to="/">Home</Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link className="nav-link" to="/about">Discover</Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link className="nav-link" to="/contact">About us</Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link className="nav-link" to="/contact">Service</Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link className="nav-link" to="/contact">Contact us</Link>
-                            </li>
-                            {/* <li className="nav-item">
-                                <Link className="nav-link" to="/contact"><button className='btn btn-block headerBtn'>Connect Wallet</button></Link>
-                            </li> */}
-                        </ul>
-                    </div>
-                </div>
-            </nav>
-        </header>
+
+        <>
+            {['xxl'].map((expand) => (
+                <Navbar key={expand} expand={expand} className="mb-3 headermenu">
+                    <Container fluid>
+                        <Navbar.Brand><img src={MainLogo} alt="logo" /></Navbar.Brand>
+                        <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
+                        <Navbar.Offcanvas
+                            id={`offcanvasNavbar-expand-${expand}`}
+                            aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
+                            placement="end"
+                        >
+                            <Offcanvas.Header closeButton>
+                                <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
+                                    <img src={MainLogo} alt="logo" />
+                                </Offcanvas.Title>
+                            </Offcanvas.Header>
+                            <Offcanvas.Body>
+                                <Nav className="justify-content-end flex-grow-1 pe-3">
+                                    <Nav.Link to="/">Home</Nav.Link>
+                                    <Nav.Link href="#/">Discover</Nav.Link>
+                                    <Nav.Link href="#/">About us</Nav.Link>
+                                    <Nav.Link href="#/">Service</Nav.Link>
+                                    <Nav.Link href="#/">Contact us</Nav.Link>
+                                </Nav>
+
+                            </Offcanvas.Body>
+                        </Navbar.Offcanvas>
+                    </Container>
+                </Navbar>
+            ))}
+        </>
+
     )
 }
